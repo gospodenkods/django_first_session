60dda06091fe886180fbb1440afaf40fde16c6f8
gospodenkods <gospodenkods@mail.ru>
2021-07-01 10:40:50 +0300

    lesson_3

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -4,6 +4,8 @@ from django.conf import settings
 import functools
 import operator
 
+from mainapp.models import Product
+
 
 def convertTuple(tup):
     str = functools.reduce(operator.add, (tup))
@@ -19,8 +21,25 @@ def products(request):
     title = 'продукты/каталог'
     t = convertTuple(settings.STATICFILES_DIRS) + "\menu_json"
     links_menu = getjson(t.replace("\\", "/"))
+    products = Product.objects.all()
     context = {
         'title': title,
         'links_menu': links_menu,
+        'products': products,
     }
     return render(request=request, template_name='mainapp/products.html', context=context)
+
+# def products(request):
+#     title = 'продукты/каталог'
+#     links_menu = [
+#         {'href': 'products_all', 'name': 'все'},
+#         {'href': 'products_home', 'name': 'дом'},
+#         {'href': 'products_office', 'name': 'офис'},
+#         {'href': 'products_modern', 'name': 'модерн'},
+#         {'href': 'products_classic', 'name': 'классика'},
+#     ]
+#     context = {
+#         'title': title,
+#         'links_menu': links_menu,
+#     }
+#     return render(request=request, template_name='mainapp/products.html', context=context)
